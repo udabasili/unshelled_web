64cddc99d298293e313b84ff0413ade7bdb6492c
Udbasili <33573587+udabasili@users.noreply.github.com>
2023-02-05 07:57:07 +0100

    Added __dirname variable for ES6

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
-import express from 'express'
-import orderRoute from './orders/OrdersRouter.js'
-import sellerRoute from './seller/SellerRouter.js'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import path from 'path'
+import express from 'express';
+import orderRoute from './orders/OrdersRouter.js';
+import sellerRoute from './seller/SellerRouter.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import * as url from 'url';
 
-dotenv.config()
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const app = express()
+dotenv.config();
 
-app.use(cors())
-app.use(express.json())
-app.use(orderRoute)
-app.use(sellerRoute)
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(orderRoute);
+app.use(sellerRoute);
 app.use(function (_req, _res, next) {
-  next({ status: 404 })
-})
+	next({ status: 404 });
+});
 
 app.use(function (error, req, res, next) {
-  return res.status(error.status || 500).json({
-    success: false,
-    message: error.message
-  })
-})
+	return res.status(error.status || 500).json({
+		success: false,
+		message: error.message,
+	});
+});
 
-app.use(express.static(path.join('../public/build')))
+app.use(express.static(path.join(__dirname, '../public/build')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/build/index.html'))
-})
+	res.sendFile(path.join(__dirname, '../public/build/index.html'));
+});
 
-export default app
+export default app;
