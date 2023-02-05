30d90a5a58ce18b4854d5892ef441cec7f814c90
Udbasili <33573587+udabasili@users.noreply.github.com>
2023-02-05 08:17:40 +0100

    Added client build

--- src/app.js
+++ src/app.js
@@ -1,36 +1,33 @@
-import express from 'express';
-import orderRoute from './orders/OrdersRouter.js';
-import sellerRoute from './seller/SellerRouter.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import path from 'path';
-import * as url from 'url';
+import express from 'express'
+import orderRoute from './orders/OrdersRouter.js'
+import sellerRoute from './seller/SellerRouter.js'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import path from 'path'
+import * as url from 'url'
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(orderRoute);
-app.use(sellerRoute);
+app.use(cors())
+app.use(express.json())
+app.use(orderRoute)
+app.use(sellerRoute)
 
-app.use(express.static(path.join(__dirname, '../public/build')));
+app.use(express.static(path.join(__dirname, '../build')))
+console.log(path.join(__dirname, '../build'))
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/build/index.html'));
-});
-
-app.use(function (_req, _res, next) {
-	next({ status: 404 });
-});
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
 
 app.use(function (error, req, res, next) {
-	return res.status(error.status || 500).json({
-		success: false,
-		message: error.message,
-	});
-});
+  return res.status(error.status || 500).json({
+    success: false,
+    message: error.message
+  })
+})
 
-export default app;
+export default app
